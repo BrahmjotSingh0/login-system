f76c9ce52feecb3397357dcce74303e960c382e5
brahmjot singh <brahmjots111@gmail.com>
2024-02-10 14:57:14 +0530

    fix database name

--- register.php
+++ register.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATAB_NAME = 'phplogin';
+$DATABASE_NAME = 'phplogin';
 
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
