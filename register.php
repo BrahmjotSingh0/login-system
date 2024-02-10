628bfe5aea18fed4ba9fe7f5cd45d786ab8b0225
brahmjot singh <brahmjots111@gmail.com>
2024-02-10 14:47:00 +0530

    fix main to email

--- register.php
+++ register.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-
-$DATABASE_HOST = 'localhost'; 
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_HOST = 'localhost';
+$DATABASEUSER = 'root';$DATABASEPASS = '';
+$DATAB_NAME = 'phplogin';
 
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
@@ -13,14 +11,14 @@ if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-if ( !isset($_POST['username'], $_POST['password'], $_POST['mail']) ) {
+if ( !isset($_POST['username'], $_POST['password'], $_POST['email']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill all the fields!');
 }
 
-if ($stmt = $con->prepare('SELECT id FROM accounts WHERE username = ? OR mail = ?')) {
+if ($stmt = $con->prepare('SELECT id FROM accounts WHERE username = ? OR email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('ss', $_POST['username'], $_POST['mail']);
+	$stmt->bind_param('ss', $_POST['username'], $_POST['email']);
 	$stmt->execute();
 	$stmt->store_result();
 
@@ -29,9 +27,9 @@ if ($stmt = $con->prepare('SELECT id FROM accounts WHERE username = ? OR mail = 
 	} else {
 		if ($password = password_hash($_POST['password'], PASSWORD_DEFAULT)) {
 			// Insert new account into the database
-			if ($stmt = $con->prepare('INSERT INTO accounts (username, password, mail) VALUES (?, ?, ?)')) {
+			if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
 				// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-				$stmt->bind_param('sss', $_POST['username'], $password, $_POST['mail']);
+				$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
 				$stmt->execute();
 				echo 'Registration successful! You can now login.';
 			} else {
