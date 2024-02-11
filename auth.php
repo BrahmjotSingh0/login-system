68c6639f1bd1f402bee5d561029c81848928da6e
brahmjot singh <brahmjots111@gmail.com>
2024-02-11 19:35:39 +0530

    allow email to login

--- auth.php
+++ auth.php
@@ -18,9 +18,9 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 	exit('Please fill both the username and password fields!');
 }
 
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ? OR email = ?')) {
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username and email are strings so we use "s"
+    $stmt->bind_param('ss', $_POST['username'], $_POST['username']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
